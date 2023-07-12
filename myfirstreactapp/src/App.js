8372979c16a48051c098e99f1e94fbd6488f94fa
Alana Zinkin <alana.zinkin@duke.edu>
2023-07-12 17:37:21 -0400

    finished basic nav bar

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import headshot from './assets/FINALheadshotJuly2023.jpg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path ="/" exact component={Home}/>
+        </Routes>
+      </Router>
+
+      <h1>Alana Zinkin</h1>
+      <img src={headshot} alt='img unavailable at this time'/>
     </div>
   );
 }
