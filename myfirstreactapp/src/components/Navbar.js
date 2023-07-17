c7ef297b5bd0dcd69b1836a9b8acf3e81dade608
Alana Zinkin <alana.zinkin@duke.edu>
2023-07-17 15:23:07 -0400

    finished navbar

--- myfirstreactapp/src/components/Navbar.js
+++ myfirstreactapp/src/components/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
 
 function Navbar() {
+    const [openLinks, setOpenLinks] = useState(false)
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks)
+    };
+
   return (
     <div className = 'navbar'>
-        <div className = 'leftSide'>
-
+        <div className = 'leftSide' id={openLinks ? "open" : "close" }>
+            <div className = 'hiddenLinks'>
+                <li><Link to='/'> Home </Link></li>
+                <li><Link to='/about'> About Alana </Link></li>
+                <li><Link to='/projects'> Projects </Link></li>
+                <li><Link to='/experiences'> Experiences </Link></li>
+                <li><Link to='/contact'> Contact </Link></li>
+                <li><Link to='/FINALresume.pdf'> Resume </Link></li>
+            </div>
         </div>
         <div className = 'rightSide'>
             <Link to='/'> Home </Link>
@@ -15,6 +28,9 @@ function Navbar() {
             <Link to='/experiences'> Experiences </Link>
             <Link to='/contact'> Contact </Link>
             <Link to='/FINALresume.pdf'> Resume </Link>
+            <button onClick={toggleNavbar}>
+                <ReorderIcon />
+            </button>
         </div>
     </div>
   )
