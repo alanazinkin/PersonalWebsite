8e3c9dbfac242e00a88bd411d7dca7e4b934056a
Alana Zinkin <alana.zinkin@duke.edu>
2023-09-09 09:43:50 -0400

    made spelling changes

--- myfirstreactapp/src/pages/About.js
+++ myfirstreactapp/src/pages/About.js
@@ -8,18 +8,16 @@ function About() {
         <p className='aboutmep1'>I started learning computer science
         right before my freshman year through the Codecademy “Learn Java”
         course. I fell in love with the experience of using code to solve problems.</p>
-        <p className='aboutmep2'>I always beleived taht I would enter the medical field and eventually
+        <p className='aboutmep2'>I always beleived that I would enter the medical field and eventually
         become a doctor, but soon realized that I could reach a larger patient
         population through improvements in medical technology. </p>
         <p className='aboutmep3'>I enrolled in Duke's CS201: Data Structures and Algorithms, and have 
         continued exploring other CS courses ever since. Now, I am learning about databases, discrete math, and javascript through Duke courses and my own 
         personal projects, such as this website!</p>
         <p className='aboutmep4'>I was raised on Long Island, New York, and
-         only recently moved to North Carolina to attend Duke. When I’m not typing away at a computer or prototyping
-          in Figma, I am either working as a tennis instructor, traveling (I most recently visited Iceland ad Greece),
+         only recently moved to North Carolina to attend Duke. When I’m not typing away at a computer, I am either working as a tennis instructor, traveling (I most recently visited Iceland ad Greece),
           eating ice cream, learning how to cook a new vegetarian recipe, 
           relaxing with family and friends at the beach, or taking a walk while listening to a podcast!
-
 
         </p>
       </div>
